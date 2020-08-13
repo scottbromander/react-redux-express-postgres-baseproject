3576e9852b2ce260041e1e01f2aba40c62909e9a
Scott Bromander <scottbromander@gmail.com>
2020-08-13 12:53:09 -0500

    setting up server with strategy

--- server/app.js
+++ server/app.js
@@ -1,15 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const compression = require('compression');
+const session = require('cookie-session');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
+app.use(compression);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// TODO: EVENTUALLY PASSPORT SESSION WHATNOT HERE
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    key: 'user',
+    resave: 'false',
+    saveUninitialized: false,
+    maxAge: 60 * 60 * 1000 * 24 * 30, // 30 days
+    secure: false,
+  })
+);
 
-app.use(express.static('server/public'));
+app.use(express.static('build'));
 
 // PRIMARY APP ROUTES
 
---
+++ server/config/passport.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+
+passport.serializeUser((user, done) => {
+  console.log('serializing user: ', user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const result = await pool.query('SELECT * FROM "user" WHERE id = $1;', [
+      id,
+    ]);
+
+    const user = result?.rows?.[0];
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, null);
+    }
+  } catch (err) {
+    console.log(`Error with query during deserializing user: ${err}`);
+    done(err, null);
+  }
+});
+
+// MOUNT AUTH SOLUTIONS HERE
+
+module.exports = passport;
